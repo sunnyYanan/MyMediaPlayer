8196b683a2bcb5c501a2e91c61f78b95b93feeba
张亚男 <1592085170@qq.com>
2013-11-07 17:06:32 +0800

    YN:Volume change, telephone monitor, play mode

--- gen/com/zhang/mymediaplayer/R.java
+++ gen/com/zhang/mymediaplayer/R.java
@@ -24,15 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int after=0x7f080005;
-        public static final int before=0x7f080003;
+        public static final int action_settings=0x7f08000d;
+        public static final int after=0x7f080006;
+        public static final int before=0x7f080004;
         public static final int controlButton=0x7f080002;
-        public static final int fileName=0x7f080006;
-        public static final int filePath=0x7f080007;
+        public static final int fileName=0x7f08000b;
+        public static final int filePath=0x7f08000c;
+        public static final int first=0x7f080003;
         public static final int introduction=0x7f080000;
-        public static final int play=0x7f080004;
+        public static final int last=0x7f080007;
+        public static final int listCycle=0x7f080010;
+        public static final int listSeq=0x7f080011;
+        public static final int oneCycle=0x7f08000f;
+        public static final int play=0x7f080005;
+        public static final int playMode=0x7f08000e;
+        public static final int random=0x7f080012;
         public static final int seekBar=0x7f080001;
+        public static final int volume=0x7f080009;
+        public static final int volumeControl=0x7f080008;
+        public static final int volumeValue=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,12 +53,18 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int after=0x7f050006;
+        public static final int after=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int before=0x7f050004;
+        public static final int first=0x7f050008;
         public static final int hello_world=0x7f050002;
         public static final int introduction=0x7f050003;
-        public static final int play=0x7f050005;
+        public static final int last=0x7f050009;
+        public static final int listCycle=0x7f05000b;
+        public static final int listSeq=0x7f05000c;
+        public static final int oneCycle=0x7f05000a;
+        public static final int play=0x7f050006;
+        public static final int volumeShow=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/zhang/mymediaplayer/MainActivity.java
+++ src/com/zhang/mymediaplayer/MainActivity.java
@@ -6,13 +6,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.os.Handler;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -22,11 +31,13 @@ import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
-	Button previous, play, next;
+	Button previous, play, next, first, last;
 	ListView list;
-	SeekBar sb;
+	SeekBar sb, volume;
+	TextView volumeValue;
 	Handler hd = new Handler();
 	int curPlay;
 	boolean isPlaying = false;
@@ -34,6 +45,10 @@ public class MainActivity extends ListActivity {
 	List<String> fileName;
 	List<String> filePath;
 	List<Map<String, String>> fileList;
+	int playMode = 1;
+	AudioManager am;
+	int currentVolume;
+	int maxVolume;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +59,57 @@ public class MainActivity extends ListActivity {
 		previous = (Button) this.findViewById(R.id.before);
 		play = (Button) this.findViewById(R.id.play);
 		next = (Button) this.findViewById(R.id.after);
+		first = (Button) this.findViewById(R.id.first);
+		last = (Button) this.findViewById(R.id.last);
 		list = (ListView) findViewById(android.R.id.list);
 		sb = (SeekBar) this.findViewById(R.id.seekBar);
+		volume = (SeekBar) this.findViewById(R.id.volume);
+		volumeValue = (TextView) this.findViewById(R.id.volumeValue);
+
 		findSongs();
+
+		if (fileList.size() == 0) {
+			previous.setEnabled(false);
+			next.setEnabled(false);
+			play.setEnabled(false);
+		}
 		previous.setOnClickListener(new MyButtonListener());
 		next.setOnClickListener(new MyButtonListener());
 		play.setOnClickListener(new MyButtonListener());
+		first.setOnClickListener(new MyButtonListener());
+		last.setOnClickListener(new MyButtonListener());
 		list.setOnItemClickListener(new MyListListener());
+
+		am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+		maxVolume = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+		volume.setMax(maxVolume);
+		currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC); // 获取当前值
+		volume.setProgress(currentVolume);
+		volumeValue.setText(currentVolume * 100 / maxVolume + " %");
+		volume.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+				// TODO Auto-generated method stub
+				am.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
+				currentVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC); // 获取当前值
+				volume.setProgress(currentVolume);
+				volumeValue.setText(currentVolume * 100 / maxVolume + " %");
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+		});
 		sb.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
@@ -73,6 +132,69 @@ public class MainActivity extends ListActivity {
 		});
 		// System.out.println(this.getExternalCacheDir());// null
 		// System.out.println(this.getFilesDir());
+
+		// 意图过滤器
+		IntentFilter filter = new IntentFilter();
+
+		// 播出电话暂停音乐播放
+		filter.addAction("android.intent.action.NEW_OUTGOING_CALL");
+		registerReceiver(new PhoneListener(), filter);
+
+		// 创建一个电话服务
+		TelephonyManager manager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+
+		// 监听电话状态，接电话时停止播放
+		manager.listen(new MyPhoneStateListener(),
+				PhoneStateListener.LISTEN_CALL_STATE);
+
+	}
+
+	/*
+	 * 监听电话状态
+	 */
+	private final class MyPhoneStateListener extends PhoneStateListener {
+		public void onCallStateChanged(int state, String incomingNumber) {
+			mp.pause();
+		}
+	}
+
+	/*
+	 * 收到广播时暂停
+	 */
+	private final class PhoneListener extends BroadcastReceiver {
+		public void onReceive(Context context, Intent intent) {
+			mp.pause();
+		}
+	}
+
+	/*
+	 * 恢复播放
+	 * 
+	 * @see android.app.Activity#onResume()
+	 */
+	protected void onResume() {
+		super.onResume();
+		mp.start();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.oneCycle:
+			playMode = 1;
+			break;
+		case R.id.listCycle:
+			playMode = 2;
+			break;
+		case R.id.listSeq:
+			playMode = 3;
+			break;
+		case R.id.random:
+			playMode = 4;
+			break;
+		}
+		return true;
 	}
 
 	public class MyListListener implements OnItemClickListener {
@@ -161,12 +283,39 @@ public class MainActivity extends ListActivity {
 
 	private void next() {
 		// TODO Auto-generated method stub
-		if (curPlay == fileName.size() - 1) {
-			curPlay = 0;
-		} else {
-			curPlay += 1;
+		if (playMode == 1) {
+			play();
+		} else if (playMode == 2) {
+			if (curPlay == fileName.size() - 1) {
+				curPlay = 0;
+			} else {
+				curPlay += 1;
+			}
+			play();
+		} else if (playMode == 3) {
+			if (curPlay == fileName.size() - 1) {
+				// curPlay = 0;
+				isPlaying = false;
+				changePlayButtonShow();
+			} else {
+				curPlay += 1;
+				play();
+			}
+		} else if (playMode == 4) {
+			curPlay = getRandomSong();
+			play();
 		}
-		play();
+
+	}
+
+	private int getRandomSong() {
+		// TODO Auto-generated method stub
+		Random random = new Random();
+//		for (int i = 0; i < fileList.size(); i++) {
+			int num = Math.abs(random.nextInt()) % fileList.size();
+			System.out.println(num);
+//		}
+		return num;
 	}
 
 	private void changePlayButtonShow() {
@@ -181,6 +330,7 @@ public class MainActivity extends ListActivity {
 	private void findSongs() {
 		// TODO Auto-generated method stub
 		File file = this.getFilesDir();
+		System.out.println(file.toString());
 		// System.out.println(path);
 		fileName = new ArrayList<String>();
 		filePath = new ArrayList<String>();
@@ -214,40 +364,50 @@ public class MainActivity extends ListActivity {
 			// Log.i("haha", "meme");
 			switch (arg0.getId()) {
 			case R.id.before:
-				if(curPlay==0)
-					curPlay=fileName.size()-1;
+				if (curPlay == 0)
+					curPlay = fileName.size() - 1;
 				else
 					curPlay--;
 				play();
 				break;
 			case R.id.play:
-				if(isPlaying) {
+				if (isPlaying) {
 					mp.pause();
-				}else {
-					if(mp== null)
+					isPlaying = false;
+				} else {
+					if (mp == null)
 						play();
-					else
+					else {
 						mp.start();
+						isPlaying = true;
+					}
+
 				}
-				isPlaying = !isPlaying;
 				changePlayButtonShow();
 				break;
 			case R.id.after:
-				if(curPlay==fileName.size()-1)
-					curPlay= 0;
+				if (curPlay == fileName.size() - 1)
+					curPlay = 0;
 				else
 					curPlay++;
 				play();
 				break;
+			case R.id.first:
+				curPlay = 0;
+				play();
+				break;
+			case R.id.last:
+				curPlay = fileList.size() - 1;
+				play();
+				break;
 			}
 		}
-
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
